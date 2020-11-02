cd8d4f2f2127a3a6b2aeea2fce41f4ebdd38cbeb
Datorium <hello@datorium.lv>
2020-11-02 18:31:01 +0200

    version 18:30

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,49 @@ namespace Calculator202010
         {
             InitializeComponent();
         }
+
+        private void NumericButtonClick(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+
+            if (Display.Text == "0")
+            {
+                Display.Text = button.Text;
+            }
+            else
+            {
+                Display.Text += button.Text;
+            }
+        }
+
+        private void buttonPeriod_Click(object sender, EventArgs e)
+        {
+            if (!Display.Text.Contains("."))
+            {
+                Display.Text += ".";
+            }            
+        }
+
+        private void buttonBackspace_Click(object sender, EventArgs e)
+        {
+            Display.Text = Display.Text.Remove(Display.Text.Length - 1);
+
+            if(Display.Text.EndsWith("."))
+            {
+                Display.Text = Display.Text.Remove(Display.Text.Length - 1);
+            }
+
+            if (Display.Text.Length == 0)
+            {
+                Display.Text = "0";
+            }
+        }
+
+        private void buttonSign_Click(object sender, EventArgs e)
+        {
+            double number = Convert.ToDouble(Display.Text);
+            number *= -1;
+            Display.Text = Convert.ToString(number);
+        }
     }
 }
